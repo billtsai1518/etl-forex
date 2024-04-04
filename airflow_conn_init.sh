5d5d6be098eb6ec3a2db7f1ba5276a6db02bbf23
yktsai <billtsai1518@gmail.com>
2024-04-04 17:49:22 +0800

    Add init airflow connection script

---
+++ airflow_conn_init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# init airflow connections
+
+
+airflow connections add --conn-type http --conn-host https://fcsapi.com/api-v3/forex/latest?symbol=USD/TWD,JPY/TWD&access_key=YOUR_KEY is_api_available
+
+airflow connections add --conn-type postgres --conn-host postgres --conn-login airflow --conn-password airflow --conn-port 5432 postgres
